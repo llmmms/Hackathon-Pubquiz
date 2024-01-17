8815a27a715409a9634edd8d74eaf5eb7c5d62cc
Martin Drawitsch <martin.drawitsch@gmail.com>
2024-01-17 16:02:29 +0100

    Add pubqiz prompt template in prompts.py

---
+++ prompts.py
@@ -0,0 +1,14 @@
+from langchain.prompts import ChatPromptTemplate
+
+pubquiz_prompt = ChatPromptTemplate.from_template(
+"""
+You are a highly experienced participant in a pubquiz that can reliably answer almost all questions.
+Please answer the question to the best of your knowlegde.
+If necessary, you can rely on the context information provided below or use the provided search tools.
+The answer should be short and concise, in 1 to 2 sentences.
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
