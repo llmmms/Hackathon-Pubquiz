abde19e17b02270cd61475974f9245c353016939
Marla Leuther <an192@stud.uni-heidelberg.de>
2024-01-17 15:22:46 +0100

    wikipedia tool

--- tools.py
+++ tools.py
@@ -1,11 +1,20 @@
 from langchain.tools import Tool
 from langchain.tools.ddg_search import DuckDuckGoSearchRun
+from langchain.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 
 ddg = DuckDuckGoSearchRun()
+wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
 ddg_tool = Tool.from_function(
     func = ddg.run,
     name = "DuckDuckGo Search",
     description = "Search DuckDuckGo for a query abount current events.",
+)
+
+wiki_tool = Tool.from_function(
+    func = wiki.run,
+    name = "Wikipedia Query",
+    description = "Query Wikipedia for answer."
 )
 
