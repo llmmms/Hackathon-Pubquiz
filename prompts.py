d7147a0fcbc5ad44c12427bd4638d406094dda18
Martin Drawitsch <martin.drawitsch@gmail.com>
2024-01-17 18:15:02 +0100

    Improve prompt

--- prompts.py
+++ prompts.py
@@ -6,6 +6,7 @@ You are a highly experienced participant in a pubquiz that can reliably answer a
 Please answer the question to the best of your knowlegde.
 If necessary, you can rely on the context information provided below or use the provided search tools.
 The answer should be short and concise, in 1 to 2 sentences.
+If you are unsure, just give best guess.
 
 <context>
 {context}
