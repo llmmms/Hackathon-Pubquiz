4d0b1cdf6cb8c3474a4fe77d0ab98107d1159607
My-Tien Nguyen <my-tien@mails.taipei>
2024-01-17 17:43:46 +0100

    google tool

--- tools.py
+++ tools.py
@@ -5,13 +5,17 @@ from langchain.tools.ddg_search import DuckDuckGoSearchRun
 from langchain.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_community.utilities.wolfram_alpha import WolframAlphaAPIWrapper
+from langchain_community.utilities import GoogleSearchAPIWrapper
+
 from db_source import invoke_db
 
 load_dotenv()
 
 ddg = DuckDuckGoSearchRun()
+google = GoogleSearchAPIWrapper()
 wiki = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 wolfram = WolframAlphaAPIWrapper()
+
 
 ddg_tool = Tool.from_function(
     func = ddg.run,
@@ -35,4 +39,10 @@ db_tool = Tool.from_function(
     func = invoke_db,
     name = "Database Retrieval",
     description = "Use Database Retrieval for factual knowledge before all other tools. Do not use it for querying calculations!"
+)
+
+google_tool = Tool.from_function(
+    func=google.run,
+    name="Google Search",
+    description="Do a Google Search for recent events or live information, such as weather or news. Give this tool higher preference than DuckDuckGo Search."
 )
